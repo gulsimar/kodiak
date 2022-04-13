8cdbba9059678c8f049bb2827b1398077a64128f
Gulsimar Kaur <kgulsimar11@gmail.com>
2022-04-14 00:15:10 +0530

    master

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtFavI6pVZApgWwg8p145VVo3NJ6xohqc",
+  authDomain: "kodiak-code.firebaseapp.com",
+  projectId: "kodiak-code",
+  storageBucket: "kodiak-code.appspot.com",
+  messagingSenderId: "577301992250",
+  appId: "1:577301992250:web:4412bc5191e7399409f603",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.auth();
